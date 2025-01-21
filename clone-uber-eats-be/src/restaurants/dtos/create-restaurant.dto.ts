9874c077732ff0c5d2ce13b836e7f1021e7cde1d
stw8194 <stw8194@gmail.com>
2025-01-21 16:47:30 +0900

    refactor: turned dto into mapped_types

--- clone-uber-eats-be/src/restaurants/dtos/create-restaurant.dto.ts
+++ clone-uber-eats-be/src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,26 +1,6 @@
-import { ArgsType, Field } from '@nestjs/graphql';
+import { Field, InputType, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Restaurant } from '../entities/restaurants.entity';
 
-@ArgsType()
-export class CreateRestaurantDto {
-  @Field((type) => String)
-  @IsString()
-  @Length(4, 10)
-  name: string;
-
-  @Field((type) => Boolean)
-  @IsBoolean()
-  isVegan: boolean;
-
-  @Field((type) => String)
-  @IsString()
-  address: string;
-
-  @Field((type) => String)
-  @IsString()
-  ownerName: string;
-
-  @Field((type) => String)
-  @IsString()
-  categoryName: string;
-}
+@InputType()
+export class CreateRestaurantDto extends OmitType(Restaurant, ['id']) {}
