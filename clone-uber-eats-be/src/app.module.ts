ed618ba14d783c9bb029ebcfeb8908a39942d2f4
stw8194 <stw8194@gmail.com>
2025-01-25 16:14:52 +0900

    feat: feat. for create account     - add basic settings to app module     - create users module, resolver, service, entity     - create common module, core entity to house everything shared across the entire app.     - creaue account method

--- clone-uber-eats-be/src/app.module.ts
+++ clone-uber-eats-be/src/app.module.ts
@@ -1,7 +1,49 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'dev'
+          ? '.env.development.local'
+          : '.env.test.local',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
+      entities: [User],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ clone-uber-eats-be/src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { CreateAccountInput } from './dtos/create-account.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly users: Repository<User>,
+  ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+    try {
+      const exists = await this.users.findOneBy({ email });
+      if (exists) {
+        return { ok: false, error: 'There is a user with that email already' };
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: "Couldn't create account" };
+    }
+  }
+}
