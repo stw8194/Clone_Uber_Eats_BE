cc62eb490e562be80828dcf23fbf8bebca346076
stw8194 <stw8194@gmail.com>
2025-02-09 17:28:40 +0900

    feat: delete verification after verified

--- clone-uber-eats-be/src/users/users.service.ts
+++ clone-uber-eats-be/src/users/users.service.ts
@@ -124,7 +124,8 @@ export class UserService {
       });
       if (verification) {
         verification.user.verified = true;
-        this.users.save(verification.user);
+        await this.users.save(verification.user);
+        await this.verification.delete(verification.id);
         return { ok: true };
       }
       return { ok: false, error: 'Verification not found.' };
