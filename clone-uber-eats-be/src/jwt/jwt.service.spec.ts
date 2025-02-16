564d5700aac73589c6c3ea1354e2f6e24622b288
stw8194 <stw8194@gmail.com>
2025-02-16 11:24:07 +0900

    test: finished sign unit test

--- clone-uber-eats-be/src/jwt/jwt.service.spec.ts
+++ clone-uber-eats-be/src/jwt/jwt.service.spec.ts
@@ -1,7 +1,13 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { JwtService } from './jwt.service';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 const TEST_KEY = 'testKey';
 
 describe('JwtService', () => {
@@ -21,6 +27,15 @@ describe('JwtService', () => {
   it('be defiend', () => {
     expect(service).toBeDefined();
   });
-  it.todo('sign');
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toBeCalledTimes(1);
+      expect(jwt.sign).toBeCalledWith({ id: ID }, TEST_KEY);
+    });
+  });
+
   it.todo('verify');
 });
