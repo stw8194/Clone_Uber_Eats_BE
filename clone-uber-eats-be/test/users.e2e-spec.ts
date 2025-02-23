b2fd716f7fa2c01cec0ae3ff3c14d12a41508133
stw8194 <stw8194@gmail.com>
2025-02-23 18:09:42 +0900

    test: setting for e2e test of UserModule

---
+++ clone-uber-eats-be/test/users.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = module.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    const dataSource = app.get(DataSource);
+
+    await dataSource.dropDatabase();
+    await dataSource.destroy();
+    app.close();
+  });
+
+  it.todo('createAccount');
+  it.todo('userProfile');
+  it.todo('login');
+  it.todo('me');
+  it.todo('verifyEmail');
+  it.todo('editProfile');
+});
